34e9ac8bfc35943b518d08c994049b841fce57cd
Carson Powers <powers.carson02@gmail.com>
2024-10-26 22:28:45 -0400

    Shaved down usb communications loop

--- feather_firmware/feather.cpp
+++ feather_firmware/feather.cpp
@@ -16,19 +16,21 @@ Feather::Feather()
 }
 
 /**
- * @brief Initialize the robot's components (encoders, IMU, etc.).
+ * @brief Initialize the sensor array components (encoders, IMU, etc.).
  */
-void Feather::init() {
+void Feather::initializeFeather() {
     // Initialize both encoders
-    encoder1_.init();
-    encoder2_.init();
+    encoder1_.initializeEncoder();
+    encoder2_.initializeEncoder();
 
     // Initialize the IMU
     imu_.initializeIMU();
 
-    // Attach GPIO interrupts and delegate to the correct encoder
+    // Attach GPIO interrupts and direct to middleman function
     gpio_set_irq_enabled_with_callback(ENCODER1_PIN_A, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, gpio_callback);
+    gpio_set_irq_enabled(ENCODER1_PIN_B, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
     gpio_set_irq_enabled(ENCODER2_PIN_A, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(ENCODER2_PIN_B, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
 }
 
 /**
@@ -37,10 +39,60 @@ void Feather::init() {
  */
 void Feather::loop() {
     while (true) {
+        process_usb_communication(); // Process USB command
+        tight_loop_contents(); // Inline No-Op to keep compiler from optimizing out loop
+    }
+}
 
+void Feather::process_usb_communication() {
+    if (tud_cdc_available()) {
+        uint8_t recievedByte = tud_cdc_read_char();
 
-        // Sleep or handle other tasks
-        sleep_ms(100);
+        switch (recievedByte) {
+            case RETURN_ENCODER_BYTE:
+            {
+                // Initialize buffer for encoder positions
+                uint8_t encoder_positions_buffer[ENCODER_DATA_BUFFER_LENGTH];
+
+                // Get encoder positions
+                int32_t encoder1_position = encoder1_.get_position();
+                int32_t encoder2_position = encoder2_.get_position();
+
+                // Copy encoder positions to buffer
+                memcpy(&encoder_positions_buffer[0], &encoder1_position, sizeof(encoder1_position));
+                memcpy(&encoder_positions_buffer[4], &encoder2_position, sizeof(encoder2_position));
+
+                // Write encoder positions to USB
+                tud_cdc_write(encoder_positions_buffer, ENCODER_DATA_BUFFER_LENGTH);
+
+                // Flush write buffer
+                tud_cdc_write_flush();
+
+                break;
+            }
+
+            case RETURN_IMU_DATA_BYTE:
+            {
+                // Initialize buffer for IMU data
+                uint8_t imu_data_buffer[IMU_DATA_BUFFER_LENGTH];
+                
+                // Read IMU data registers (0x38 - 0x48)
+                // Data is 2 bytes per DOF, little endian
+                // Indicies:
+                // 0 - 5 : Accelerometer (X, Y, Z)
+                // 6 - 7 : Temperature
+                // 8 - 13 : Gyroscope (X, Y, Z)
+                imu_.readIMU(0x3B, imu_data_buffer, IMU_DATA_BUFFER_LENGTH);
+                
+                // Write IMU data buffer to USB
+                tud_cdc_write(imu_data_buffer, IMU_DATA_BUFFER_LENGTH);
+                
+                // Flush write buffer
+                tud_cdc_write_flush();
+                
+                break;
+            }
+        }
     }
 }
 
